218d1cb48a6fc1c0d99b05fe2c62b236e011a56f
Dias <dias.atudinov@gmail.com>
2025-03-17 21:59:57 +0500

    first init

---
+++ Indian Eagle/Indian Eagle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Indian Eagle
+//
+//  Created by Dias Atudinov on 17.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Indian Eagle/Indian Eagle/Indian_EagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indian_EagleApp.swift
+//  Indian Eagle
+//
+//  Created by Dias Atudinov on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Indian_EagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
