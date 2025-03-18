fb64f2cdd05618f0202b62e9edadd580151202b0
Dias <dias.atudinov@gmail.com>
2025-03-18 17:49:38 +0500

    add menu view

--- Indian Eagle/Indian Eagle/ModulsIE/MenuViewIE/MenuViewIE.swift
+++ Indian Eagle/Indian Eagle/ModulsIE/MenuViewIE/MenuViewIE.swift
@@ -8,9 +8,102 @@
 import SwiftUI
 
 struct MenuViewIE: View {
+    @State private var showAIGame = false
+    @State private var showShop = false
+    @State private var showSettings = false
+    
+//    @StateObject var shopVM = SVM()
+//    @StateObject var settingsVM = SM()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            ZStack {
+                VStack(spacing: 0) {
+                    HStack {
+                        
+                        FLBirdsView()
+                        
+                        Spacer()
+                        
+                        IEStarsView()
+                        
+                    }
+                    Spacer()
+                    
+                    HStack {
+                        Button {
+                            showAIGame = true
+                        } label: {
+                            Image(.playIconIE)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: DeviceInfoIE.shared.deviceType == .pad ?400:200)
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    HStack {
+                        Button {
+                            showShop = true
+                        } label: {
+                            Image(.shopIconIE)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: DeviceInfoIE.shared.deviceType == .pad ? 180:90)
+                        }
+                        Spacer()
+                        Button {
+                            showSettings = true
+                        } label: {
+                            Image(.settingsIconIE)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: DeviceInfoIE.shared.deviceType == .pad ? 180:90)
+                        }
+                        
+                    }
+                }.padding()
+            }
+            .background(
+                ZStack {
+                    Image(.appBgIE)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+                
+            )
+//            .onAppear {
+//                if settingsVM.musicEnabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                }
+//            }
+//            .onChange(of: settingsVM.musicEnabled) { enabled in
+//                if enabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                } else {
+//                    MusicPlayer.shared.stopBackgroundMusic()
+//                }
+//            }
+            .fullScreenCover(isPresented: $showAIGame) {
+//                GameView(shopVM: shopVM, opponentState: .ai)
+            }
+            .fullScreenCover(isPresented: $showShop) {
+//                ShopV(shopVM: shopVM)
+                
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+//                SV(settings: settingsVM)
+                
+            }
+            
+        }
+        
+        
     }
+    
 }
 
 #Preview {
