19af321209ca20aac26e1b99d96f101bdb4d0d0d
Dias <dias.atudinov@gmail.com>
2025-03-18 17:50:07 +0500

    add helper views, like, birds and stars views

--- Indian Eagle/Indian Eagle/HelpersIE/IEBirdsView.swift
+++ Indian Eagle/Indian Eagle/HelpersIE/IEBirdsView.swift
@@ -11,7 +11,7 @@ struct FLBirdsView: View {
             
             Text("\(user.birds)")
                 .font(.system(size: DeviceInfoIE.shared.deviceType == .pad ? 40:20, weight: .black))
-                .foregroundStyle(.white)
+                .foregroundStyle(.appBrown)
                 .textCase(.uppercase)
                 .offset(x: DeviceInfoIE.shared.deviceType == .pad ? 40:20, y: DeviceInfoIE.shared.deviceType == .pad ? 10:5)
         }.frame(height: DeviceInfoIE.shared.deviceType == .pad ? 140:70)
--- Indian Eagle/Indian Eagle/HelpersIE/IEStarsView.swift
+++ Indian Eagle/Indian Eagle/HelpersIE/IEStarsView.swift
@@ -8,24 +8,24 @@
 
 import SwiftUI
 
-struct FLBirdsView: View {
+struct IEStarsView: View {
     @StateObject var user = IEUser.shared
     var body: some View {
         ZStack {
-            Image(.birdsBgIE)
+            Image(.starsBgIE)
                 .resizable()
                 .scaledToFit()
                 
             
-            Text("\(user.birds)")
+            Text("\(user.stars)")
                 .font(.system(size: DeviceInfoIE.shared.deviceType == .pad ? 40:20, weight: .black))
-                .foregroundStyle(.white)
+                .foregroundStyle(.appBrown)
                 .textCase(.uppercase)
-                .offset(x: DeviceInfoIE.shared.deviceType == .pad ? 40:20, y: DeviceInfoIE.shared.deviceType == .pad ? 10:5)
+                .offset(x: DeviceInfoIE.shared.deviceType == .pad ? -40:-20, y: DeviceInfoIE.shared.deviceType == .pad ? 10:5)
         }.frame(height: DeviceInfoIE.shared.deviceType == .pad ? 140:70)
     }
 }
 
 #Preview {
-    FLBirdsView()
+    IEStarsView()
 }
