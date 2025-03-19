02037fe5b536053c52b71b4967a703915a9f68fc
Dias <dias.atudinov@gmail.com>
2025-03-19 21:16:14 +0500

    new files

---
+++ Indian Eagle/Indian Eagle/ModulsIE/SettingsViewIe/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Indian Eagle
+//
+//  Created by Dias Atudinov on 19.03.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
